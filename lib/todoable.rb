fe6acd171340be7e22ed500a3c2ac58124003bbd
Rafael George <george.rafael@gmail.com>
2018-10-15 08:20:45 -0400

    Add Adapters::HTTP
    This will allow client to be totally abstracted from the http interactions done
    against Faraday or any other HTTP library.

--- lib/todoable.rb
+++ lib/todoable.rb
@@ -1,9 +1,56 @@
 require "todoable/version"
 require "dotenv/load"
+require "faraday"
+require "faraday_middleware"
 require_relative "./todoable/client"
 
 module Todoable
+  BASE_API_URL = ENV.fetch("BASE_API_URL", "http://todoable.teachable.tech/")
+
+  module Adapters
+    class HTTP
+      def self.with_credentials(username:, password:, http_client: Faraday)
+        conn = http_client.new(url: BASE_API_URL) do |faraday|
+          faraday.request :json
+          faraday.response :json
+          faraday.adapter http_client.default_adapter
+        end
+        conn.basic_auth username, password
+        new(connection: conn)
+      end
+
+      def initialize(connection:)
+        @connection = connection
+        @token = retrieve_token
+      end
+
+      def post(url:, params:)
+        connection.post(url, params, headers)
+      end
+
+      def get(url:)
+        connection.get(url, {}, headers).body
+      end
+
+      private
+
+      attr_reader :connection, :token
+
+      def headers
+        {"Authorization" => "Token token=\"#{token}\""}
+      end
+
+      def retrieve_token
+        response = connection.post("/api/authenticate")
+        response.body["token"]
+      end
+    end
+  end
+
   def self.authenticate!(username:, password:)
-    Client.new(username: username, password: password)
+    Client.new(
+      http_adapter: Adapters::HTTP.with_credentials(username: username,
+                                                    password: password)
+    )
   end
 end
--- lib/todoable/client.rb
+++ lib/todoable/client.rb
@@ -1,81 +1,26 @@
-require "faraday"
-require "faraday_middleware"
-
 module Todoable
-  BASE_API_URL = ENV.fetch("BASE_API_URL", "http://todoable.teachable.tech/")
-  # HTTP Codes
-  HTTP_OK_CODE = 200
-  HTTP_UNPROCESSABLE_ENTITY_CODE = 422
-
-  # Errors
-  class UnprocessableEntityError < StandardError; end
-
-
   class Client
-    def initialize(username:, password:, http_client: Faraday)
-      @http_adapter = build_connection_and_authenticate_with(username, password, http_client)
+    def initialize(http_adapter:)
+      @http_adapter = http_adapter
     end
 
     def lists
-      parsed_response = make_request(
-        http_method: :get,
-        url: "/api/lists",
-        params: {},
-      )
+      parsed_response = http_adapter.get(url: "/api/lists")
       parsed_response["lists"].map do |list|
         {name: list["name"]}
       end
     end
 
     def create_list!(name:)
-      make_request(
-        http_method: :post,
-        url: "/api/lists",
-        params: build_create_list_request(name)
-      )
+      http_adapter.post(url: "/api/lists", params: build_create_list_request(name))
     end
 
     private
 
     attr_reader :http_adapter, :response
 
-    def make_request(http_method:, url:, params:)
-      @response = http_adapter.public_send(http_method, url, params, build_request_headers)
-      return response.body if successful_response?
-      raise error_class.new("Code: #{response.status}, response: #{response.body}")
-    end
-
-    def successful_response?
-      response.status == HTTP_OK_CODE
-    end
-
-    def error_class
-      case response.status
-      when HTTP_UNPROCESSABLE_ENTITY_CODE
-        UnprocessableEntityError
-      end
-    end
-
     def build_create_list_request(name)
       { "list" => {"name" => name} }
-    end
-
-    def build_request_headers
-      {"Authorization" => "Token token=\"#{token}\""}
-    end
-
-    def token
-      @_token = http_adapter.post("/api/authenticate").body['token']
-    end
-
-    def build_connection_and_authenticate_with(username, password, http_client)
-      http_adapter = http_client.new(url: BASE_API_URL) do |conn|
-        conn.request :json
-        conn.response :json
-        conn.adapter http_client.default_adapter
-      end
-      http_adapter.basic_auth username, password
-      http_adapter
     end
   end
 end
